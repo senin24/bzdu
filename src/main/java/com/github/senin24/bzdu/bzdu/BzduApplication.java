dd7754de8f41116f7f45b0e188dd8c2c10a0422c
Pavel_Senin <psenin@jtconsulting.ru>
2019-09-15 17:26:02 +0300

    first commit

---
+++ src/main/java/com/github/senin24/bzdu/bzdu/BzduApplication.java
@@ -0,0 +1,13 @@
+package com.github.senin24.bzdu.bzdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BzduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BzduApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/senin24/bzdu/bzdu/BzduApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.senin24.bzdu.bzdu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BzduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
